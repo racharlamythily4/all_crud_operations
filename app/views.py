ebc03c290e87f85115bba56f869a7268416cdedd
racharlamythily <racharlamythily4@gmail.com>
2023-08-28 21:34:07 -0500

    first commit

--- app/views.py
+++ app/views.py
@@ -8,16 +8,50 @@ from django.db.models.functions import Length
 
 def display_topic(request):
     QSTO=Topic.objects.all()
+    QSTO=Topic.objects.filter(topic_name='Cricket')
+    QSTO=Topic.objects.exclude(topic_name='Cricket')
+    QSTO=Topic.objects.all()
+    QSTO=Topic.objects.filter(topic_name__contains='ba')
+    QSTO=Topic.objects.all()
     d={'topics':QSTO}
     return render(request,'display_topic.html',d)
 
 def display_webpage(request):
     QSWO=Webpage.objects.all()
+    QSWO=Webpage.objects.filter(topic_name='Football')
+    QSWO=Webpage.objects.exclude(topic_name='Football')
+    QSWO=Webpage.objects.filter(topic_name='Kabaddi').order_by('name')
+    QSWO=Webpage.objects.all().order_by('name')
+    QSWO=Webpage.objects.all().order_by('-name')
+    QSWO=Webpage.objects.all().order_by(Length('name'))
+    QSWO=Webpage.objects.all().order_by(Length('name').desc())
+    QSWO=Webpage.objects.all()
+    QSWO=Webpage.objects.filter(name='chandu')
+    QSWO=Webpage.objects.filter(pk__gt=4)
+    QSWO=Webpage.objects.all()
+    QSWO=Webpage.objects.filter(name__startswith='n')
+    QSWO=Webpage.objects.filter(url__endswith='com')
+    QSWO=Webpage.objects.filter(name__contains='ba')
+    QSWO=Webpage.objects.filter(name__in=('shoban','chandu')) 
+    QSWO=Webpage.objects.filter(name__regex='^\w')
+    QSWO=Webpage.objects.filter(url__regex='[com]')
+    QSWO=Webpage.objects.all()
+
     d={'web':QSWO}
     return render(request,'display_webpage.html',d)
 
 def display_acessrecords(request):
     QSARO=Accessrecords.objects.all()
+    QSARO=Accessrecords.objects.filter(author__contains='a')
+    QSARO=Accessrecords.objects.filter(date__month='08')
+    QSARO=Accessrecords.objects.filter(date__year='1999')
+    QSARO=Accessrecords.objects.filter(date__year__gt='1999')
+    QSARO=Accessrecords.objects.filter(date__year__gte='1999')
+    QSARO=Accessrecords.objects.filter(date__year__lt='1999')
+    QSARO=Accessrecords.objects.filter(date__year__lte='1999')
+    QSARO=Accessrecords.objects.filter(date__day='06')
+    QSARO=Accessrecords.objects.all()
+    
     d={'records':QSARO}
     return render(request,'display_acessrecords.html',d)
 
